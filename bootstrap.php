2679524289d7442a62578d5c11dae287a6720de2
sorexalpinus <57848381+sorexalpinus@users.noreply.github.com>
2020-01-04 17:32:03 +0100

    paths added into bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,10 +6,14 @@
  * Time: 12:42
  */
 
-define('DS',DIRECTORY_SEPARATOR);
-$path = str_replace(['\\','/'],[DS,DS],realpath(__DIR__).'/src');
-$funcTestPath = str_replace(['\\','/'],[DS,DS],realpath(__DIR__).'/functional_tests');
+define('DS', DIRECTORY_SEPARATOR);
+
+$root = str_replace(['\\', '/'], [DS, DS], realpath(__DIR__));
+$srcPath = $root . '/src';
+$funcTestPath = $root . '/functional_tests';
 $loader = require 'vendor/autoload.php';
-$loader->addPsr4('Cubify\\', $path);
+$loader->addPsr4('Cubify\\', $srcPath);
 $loader->addPsr4('Cubify\\', $funcTestPath);
-define('SRC_PATH',$path);
+define('SRC_PATH', $srcPath);
+define('RSRC_PATH', $root . DS . 'resources');
+define('SQL_PATH', $root . DS . 'resources' . DS . 'sql');
