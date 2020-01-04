acaadc752451a4480fbd9a044c377cc983d9bfd5
sorexalpinus <57848381+sorexalpinus@users.noreply.github.com>
2020-01-04 17:30:44 +0100

    corrected cubify exception constructor

--- src/exceptions/CubifyException.php
+++ src/exceptions/CubifyException.php
@@ -1,12 +1,14 @@
 <?php
+
 namespace Cubify\Exceptions;
 
 use Exception;
 
 class CubifyException extends Exception
 {
-    public function __construct($message = "", $code = 0, $previous = null) {
-        parent::__construct($message = "", $code = 0,  $previous = null);
+    public function __construct($message = "", $code = 0, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
         $this->message = 'Cubify: ' . $this->message;
     }
 }
