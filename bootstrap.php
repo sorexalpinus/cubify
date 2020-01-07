797af05ede196598cca6f51c3e590d04e8f260c7
sorexalpinus <57848381+sorexalpinus@users.noreply.github.com>
2020-01-07 01:10:19 +0100

    changed gitignore and bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -5,16 +5,9 @@
  * Date: 12. 9. 2019
  * Time: 12:42
  */
-
 define('DS', DIRECTORY_SEPARATOR);
-
 define('CUBIFY_ROOT',str_replace(['\\', '/'], [DS, DS], realpath(__DIR__)));
 define('CUBIFY_SRC_PATH', CUBIFY_ROOT . DS . 'src');
 define('CUBIFY_TESTS_PATH', CUBIFY_ROOT . DS . 'tests');
 define('CUBIFY_RSRC_PATH', CUBIFY_ROOT . DS . 'resources');
 define('CUBIFY_SQL_PATH', CUBIFY_ROOT . DS . 'resources' . DS . 'sql');
-
-$loader = require 'vendor' . DS . 'autoload.php';
-
-$loader->addPsr4('Cubify\\', CUBIFY_SRC_PATH);
-$loader->addPsr4('Cubify\\Tests\\', CUBIFY_TESTS_PATH);
