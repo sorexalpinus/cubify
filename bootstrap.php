ea4f9551ad9f4324b9ac6be4532e66d21ed2f874
sorexalpinus <57848381+sorexalpinus@users.noreply.github.com>
2020-01-07 01:16:05 +0100

    further config fixes

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jhlatky
- * Date: 12. 9. 2019
- * Time: 12:42
- */
 define('DS', DIRECTORY_SEPARATOR);
 define('CUBIFY_ROOT',str_replace(['\\', '/'], [DS, DS], realpath(__DIR__)));
 define('CUBIFY_SRC_PATH', CUBIFY_ROOT . DS . 'src');
